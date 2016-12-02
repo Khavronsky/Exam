e8fe0b61099e1d3cef9283a7940332a205a1fab4
khavronsky <khavronsky@gmail.com>
2016-12-02 17:19:51 +0300

    Create CustomLinkedList

---
+++ src/CustomLinkedList/IncredibleLinkedList.java
@@ -0,0 +1,46 @@
+package CustomLinkedList;
+
+public class IncredibleLinkedList {
+    private LinkedItem currentItem;
+
+    void add(int value) {
+        currentItem = new LinkedItem(currentItem, value);
+    }
+
+
+    void add(int index, int value) {
+
+    }
+
+    int get(int index) {
+        int value = 0;
+        LinkedItem item = currentItem;
+
+        for (int i = 0; i < size() - index; i++) {
+            value = item.value;
+            item = item.nextItem;
+        }
+        return value;
+    }
+
+    int size() {
+        int counter = 0;
+        LinkedItem item = currentItem;
+        while (item != null){
+            counter++;
+            item = item.nextItem;
+        }
+        return counter;
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+        LinkedItem item = currentItem;
+        while (item != null){
+            s = item.value + " " + s;
+            item = item.nextItem;
+        }
+        return s;
+    }
+}
---
+++ src/CustomLinkedList/LinkedItem.java
@@ -0,0 +1,11 @@
+package CustomLinkedList;
+
+class LinkedItem {
+    LinkedItem nextItem;
+    int value;
+
+    LinkedItem(LinkedItem nextItem, int value) {
+        this.nextItem = nextItem;
+        this.value = value;
+    }
+}
